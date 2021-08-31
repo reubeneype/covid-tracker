a8ec3620d38ce85cc7975e00c85148906602bc48
reubeneype <63570752+reubeneype@users.noreply.github.com>
2021-08-31 23:13:52 +0530

    added country selection and handled country change

--- src/api/index.js
+++ src/api/index.js
@@ -2,9 +2,15 @@ import axios from 'axios';
 
 const url = 'https://covid19.mathdro.id/api';
 
-export const fetchData = async () => {
+export const fetchData = async (country) => {
+    let dynamicUrl = url;
+
+    if(country) {
+        dynamicUrl = `${url}/countries/${country}`;
+    }
+
     try{
-        const {data: {confirmed,recovered,deaths,lastUpdate}} = await axios.get(url);
+        const {data: {confirmed,recovered,deaths,lastUpdate}} = await axios.get(dynamicUrl);
         return {confirmed,recovered,deaths,lastUpdate};
     } catch(error){
 
@@ -23,6 +29,15 @@ export const fetchDailyData = async () => {
 
         return modifiedData;
     } catch(error) {
+        console.log(error);
+    }
+}
+
+export const fetchCountries = async () => {
+    try{
+        const {data : {countries}} = await axios.get(`${url}/countries`);
+        return countries.map((country) => country.name);
+    } catch (error) {
 
     }
 }
--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -2,6 +2,7 @@ import React, {useState,useEffect} from "react";
 import { fetchDailyData } from "../../api";
 import { Line, Bar } from "react-chartjs-2";
 
+
 import styles from './Chart.module.css'
 
 const Chart = () => {
@@ -11,8 +12,9 @@ const Chart = () => {
         const fetchAPI = async () => {
             setDailyData(await fetchDailyData());
         }
+        console.log(dailyData);
         fetchAPI();
-    });
+    },[]);
 
     const lineChart = (
         dailyData.length
@@ -23,8 +25,8 @@ const Chart = () => {
                 datasets: [{
                     data: dailyData.map(({confirmed})=> confirmed),
                     label: 'Infected',
-                    borderColor: '#333ff',
-                    fill: true
+                    borderColor: '#3333ff',
+                    fill: true,
                 },{
                     data: dailyData.map(({deaths})=> deaths),
                     label: 'Deaths',
